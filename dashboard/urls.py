1694e2ae0313f10e1741dbbb8a5ebb7d247f350d
root <root@localhost.localdomain>
2018-06-04 17:50:20 +0800

    first commit

---
+++ dashboard/urls.py
@@ -0,0 +1,9 @@
+
+from django.conf.urls import url,include
+from django.contrib import admin
+from dashboard import views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^index/', views.index),
+]
